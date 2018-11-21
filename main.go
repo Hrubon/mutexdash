f67e000073f660c27f50a753f4f8e240174ed3a3
Ondřej Hrubý <hrubon@hotmail.cz>
2018-11-21 22:39:40 +0100

    model and webserver functional

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Hrubon/mutexdash/model"
+	"github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+/* Command-line options definition */
+
+var opts struct {
+	EtcdEndpoints []string `short:"e" long:"etcd-eps" required:"true" description:"URL describing etcd REST endpoint. This option can appear multiple times."`
+	EtcdTimeout int `short:"t" long:"etcd-to" default:"5" description:"etcd receive timeout - how many seconds to wait for a response."`
+	EtcdRootNs string `short:"n" long:"etcd-root-ns" default:"/mutexes" description:"etcd root namespace - where to look for mutexes."`
+	SkipCheck bool `short:"s" long:"skip-test" description:"Skip testing the connection to the etcd upstream on startup."`
+	ListenOn string `short:"l" long:"listen-on" required:"true" description:"Socket on which to listen for incomming HTTP connections - e.g. 127.0.0.1:8080."`
+}
+
+func usage() {
+	fmt.Println("usage: ./mutexdash")
+	fmt.Println("	-e <etcd upstream endpoint list>")
+	fmt.Println("	-t <etcd receive timeout in seconds>")
+	fmt.Println("	-n <etcd mutex root namespace>")
+	fmt.Println("	-s whether to skip initial etcd upstream check")
+	fmt.Println("	-l <http listening socket - in form address:port>")
+
+}
+
+/* HTTP error-handling */
+
+func httpError(w http.ResponseWriter, httpCode int, err error) {
+	err = errors.Wrapf(err, "HTTP status %d", httpCode)
+	http.Error(w, err.Error(), httpCode)
+}
+
+func assertHttpMethod(w http.ResponseWriter, r *http.Request, method string) bool {
+	if r.Method != method {
+		msg := fmt.Sprintf("HTTP method '%s' is not allowed, use '%s' method instead", r.Method, method)
+		httpError(w, http.StatusMethodNotAllowed, errors.New(msg))
+		return false
+	}
+	return true
+}
+
+/* Main entry point - web server controller */
+
+func main() {
+	// Parse command-line options
+    _, err := flags.Parse(&opts)
+	if err != nil {
+		usage()
+		os.Exit(1)
+	}
+
+	// Init model and perform upstream check
+	model := model.NewModel(opts.EtcdEndpoints, opts.EtcdTimeout, opts.EtcdRootNs)
+	if !opts.SkipCheck {
+		err = model.TestConnection()
+		if err != nil {
+			err = errors.Wrap(err, "Exiting due to a problem with the etcd upstream")
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+	// Setup web server controller
+	http.HandleFunc("/",
+		func (w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/mutex/list", 301)
+		})
+	http.HandleFunc("/mutex/list",
+		func (w http.ResponseWriter, r *http.Request) {
+			if !assertHttpMethod(w, r, "GET") {
+				return
+			}
+			mList, err := model.ListMutexes()
+			if err != nil {
+				httpError(w, http.StatusInternalServerError, err)
+				return
+			}
+			t, err := template.ParseFiles("mutexlist.html")
+			if err != nil {
+				httpError(w, http.StatusNotFound, err)
+				return
+			}
+			t.Execute(w, mList)
+		})
+	http.HandleFunc("/mutex/unlock",
+		func (w http.ResponseWriter, r *http.Request) {
+			if !assertHttpMethod(w, r, "POST") {
+				return
+			}
+			etcdPath := r.PostFormValue("etcdPath")
+			if etcdPath == "" {
+				err := errors.New("'etcdPath' field is missing in the POST request body")
+				httpError(w, http.StatusInternalServerError, err)
+				return
+			}
+			err := model.UnlockMutex(etcdPath)
+			if err != nil {
+				httpError(w, http.StatusInternalServerError, err)
+				return
+			}
+			prevService := r.PostFormValue("prevService")
+			if prevService != "" {
+				http.Redirect(w, r, "/mutex/list#" + prevService, 303)
+			} else {
+				http.Redirect(w, r, "/mutex/list", 303)
+			}
+		})
+
+	// Start listening for HTTP requests
+	fmt.Fprintln(os.Stderr, "MutexDash v1.0")
+	log.Fatal(http.ListenAndServe(opts.ListenOn, nil))
+}
+
---
+++ model/model.go
@@ -0,0 +1,197 @@
+package model
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"go.etcd.io/etcd/client"
+	"log"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/* Model definition and initialization */
+
+type model struct{
+	etcdEndpoints []string
+	etcdTO int
+	etcdRootNs string
+}
+
+func NewModel(etcdEndpoints []string, etcdTO int, etcdRootNs string) *model {
+	return &model{
+		etcdEndpoints: etcdEndpoints,
+		etcdTO: etcdTO,
+		etcdRootNs: etcdRootNs,
+	}
+}
+
+/* Model data structures */
+
+type Mutex struct {
+	Name string
+	Hostname string
+	Timestamp time.Time
+	Description string
+	EtcdPath string
+}
+
+type Service struct {
+	Name string
+	Mutexes []*Mutex
+}
+
+/* etcd cluster connection handling */
+
+func (m *model) initEtcdClient(endpoints []string) client.KeysAPI {
+	cfg := client.Config {
+		Endpoints: endpoints,
+	}
+	c, err := client.New(cfg)
+	cli := client.NewKeysAPI(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cli
+}
+
+func (m *model) getTOContext() (context.Context, context.CancelFunc) {
+	to := time.Duration(m.etcdTO) * time.Second
+	return context.WithTimeout(context.Background(), to)
+}
+
+/* etcd tree hierarchy manipulation */
+
+/* Get the last component of the etcd key path */
+func pathLast(path string) string {
+	return path[strings.LastIndex(path, "/") + 1:]
+}
+
+/* Get descendant of given root node by its relative path from root */
+func getDesc(root *client.Node, path string) *client.Node {
+	i := strings.Index(path, "/")
+	if i == -1 {
+		for _, node := range root.Nodes {
+			if pathLast(node.Key) == path {
+				return node
+			}
+		}
+		return nil
+	} else {
+		for _, node := range root.Nodes {
+			if pathLast(node.Key) == path[:i] {
+				return getDesc(node, path[i + 1:])
+			}
+		}
+		return nil
+	}
+}
+
+/* Get the descendant's value as string */
+func getDescStrVal(root *client.Node, path string) string {
+	node := getDesc(root, path)
+	if node == nil {
+		return ""
+	} else {
+		return node.Value
+	}
+}
+
+/* Get the descendant's value as time */
+func getDescTimeVal(root *client.Node, path string) time.Time {
+	timeStr := getDescStrVal(root, path)
+	if timeStr == "" {
+		return time.Time{}
+	}
+	tStamp, err := strconv.ParseInt(timeStr, 10, 64)
+	if err != nil {
+		return time.Time{}
+	} else {
+		return time.Unix(tStamp, 0)
+	}
+}
+
+func (m *model) TestConnection() error {
+	for _, endpoint := range m.etcdEndpoints {
+		cli := m.initEtcdClient([]string{endpoint})
+		ctx, cancel := m.getTOContext()
+		defer cancel()
+		_, err := cli.Get(ctx, m.etcdRootNs, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+/* Model public API */
+
+/* List all mutexes from the root namespace and group them by services */
+func (m *model) ListMutexes() ([]*Service, error) {
+	cli := m.initEtcdClient(m.etcdEndpoints)
+	ctx, cancel := m.getTOContext()
+	defer cancel()
+	ns := m.etcdRootNs
+	resp, err := cli.Get(ctx, ns, &client.GetOptions{Recursive: true, Sort: true})
+	if err != nil {
+		if client.IsKeyNotFound(err) {
+			return nil, errors.Wrapf(err, "could not open etcd root namespace '%s'", ns)
+		} else {
+			return nil, err
+		}
+	}
+	services := make([]*Service, resp.Node.Nodes.Len())
+	for i, serviceNode := range resp.Node.Nodes {
+		if serviceNode.Dir {
+			mutexes := make([]*Mutex, 0)
+			for _, mutexNode := range serviceNode.Nodes {
+				if getDesc(mutexNode, "held") != nil {
+					mutexes = append(mutexes, &Mutex{
+						Name: pathLast(mutexNode.Key),
+						Hostname: getDescStrVal(mutexNode, "owner/hostname"),
+						Timestamp: getDescTimeVal(mutexNode, "owner/since"),
+						Description: getDescStrVal(mutexNode, "owner/help"),
+						EtcdPath: mutexNode.Key,
+					})
+				}
+			}
+			services[i] = &Service{
+				Name: pathLast(serviceNode.Key),
+				Mutexes: mutexes,
+			}
+		}
+	}
+	return services, nil
+}
+
+/* Delete (unlock) mutex from etcd cluster and all of its metadata */
+func (m *model) UnlockMutex(mutexPath string) error {
+	cli := m.initEtcdClient(m.etcdEndpoints)
+	ctx, cancel := m.getTOContext()
+	defer cancel()
+	_, err := cli.Delete(ctx, mutexPath, &client.DeleteOptions{ Recursive: true })
+	if err != nil && client.IsKeyNotFound(err) {
+		return errors.Wrapf(err, "mutex named '%s' does not exists", mutexPath)
+	}
+	return err
+}
+
+
+
+/* Dummy queries */
+
+func (m *model) DummyMutexList() []*Mutex {
+	mutexes := make([]*Mutex, 2)
+	mutexes[0] = &Mutex{ Name: "Delete", Hostname: "SRV-1234", Timestamp: time.Unix(int64(rand.Int31()), 0), Description: "Test", EtcdPath: "/mutexes/user_profile/logout/held" }
+	mutexes[1] = &Mutex{ Name: "Update", Hostname: "SRV-5678", Timestamp: time.Unix(int64(rand.Int31()), 0), Description: "Hello world", EtcdPath: "/mutexes/billing/pay_cash/held" }
+	return mutexes
+}
+
+func (m *model) DummyServiceList() []*Service {
+	services := make([]*Service, 2)
+	services[0] = &Service{ Name: "Servica 1", Mutexes: m.DummyMutexList() }
+	services[1] = &Service{ Name: "Servica 2", Mutexes: m.DummyMutexList() }
+	return services
+}
+
